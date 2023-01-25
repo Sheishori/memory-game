8c3c1cd5cdec1ce50152897e5158384e010e31c2
Sheishori <sheishori@gmail.com>
2023-01-25 16:28:55 +0100

    Add placeholder text to the scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,5 +2,11 @@ import React from 'react';
 import '../styles/Scoreboard.css';
 
 export default function Scoreboard() {
-	return <div id='scoreboard'></div>;
+	return (
+		<div id='scoreboard'>
+			<div>Current score: 0</div>
+			<div>Best score: 0</div>
+			<div>Max score: 12</div>
+		</div>
+	);
 }
