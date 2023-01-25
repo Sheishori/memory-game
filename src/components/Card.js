bea83982ee70a2d19336e0a31ceebb5ed95f3972
Sheishori <sheishori@gmail.com>
2023-01-25 16:45:46 +0100

    Add placeholder data to the Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,5 +2,13 @@ import React from 'react';
 import '../styles/Card.css';
 
 export default function Card() {
-	return <div className='card'></div>;
+	return (
+		<div className='card'>
+			<img
+				src='https://static.wikia.nocookie.net/finalfantasy/images/7/70/WoFF_Tonberry.png'
+				alt='Tonberry'
+			/>
+			<div>Tonberry</div>
+		</div>
+	);
 }
